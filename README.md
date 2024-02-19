e7deae0aff21cc3bfcd7eef4dd4cbe263c4d89c9
felipe <jonathanfelipe0111@hotmail.com>
2024-02-19 16:38:41 -0500

    Código para construir una aplicación web segura con Node.js, Express y MongoDB. Incluye configuración del proyecto, conexión a la base de datos, definición de modelos, implementación de rutas y controladores, y autenticación de usuarios.

---
+++ README.md
@@ -0,0 +1,30 @@
+# SecureNodeAuthApp
+
+Este repositorio contiene el código fuente de una aplicación web desarrollada con Node.js, Express y MongoDB, centrada en la autenticación segura de usuarios.
+
+## Descripción
+
+Este proyecto es una guía paso a paso para construir una aplicación web segura utilizando tecnologías modernas de desarrollo web. Desde la configuración inicial del proyecto hasta la implementación de autenticación de usuarios, este tutorial detallado proporciona una base sólida para el desarrollo de aplicaciones web seguras y escalables.
+
+## Características
+
+- Configuración del proyecto con Node.js y Express.
+- Conexión a una base de datos MongoDB.
+- Definición de modelos de datos utilizando Mongoose.
+- Implementación de rutas y controladores para la gestión de usuarios.
+- Autenticación de usuarios utilizando bcrypt y JWT (JSON Web Tokens).
+
+## Instalación
+
+1. Clona este repositorio: `git clone https://github.com/tu_usuario/SecureNodeAuthApp.git`
+2. Instala las dependencias: `npm install`
+3. Configura las variables de entorno creando un archivo `.env` basado en el archivo `.env.example`.
+4. Inicia la aplicación: `npm start`
+
+## Contribución
+
+¡Las contribuciones son bienvenidas! Si encuentras algún problema o tienes alguna sugerencia, por favor abre un issue o envía un pull request.
+
+## Licencia
+
+Este proyecto está bajo la Licencia MIT.
---
+++ routes/UserRoutes.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const UserSchema = require('../models/User');
+const UserController = require('../controllers/UserController'); //Importando el controllador
+const userController = new UserController(); // creando una instancia de ese controlador
+
+router.get('/user', userController.validateToken, async (req, res) => {
+    //Traer todos los usuarios
+    // Esta ruta obtiene todos los usuarios de la base de datos
+    // usando el método find() del modelo UserSchema.
+    let users = await UserSchema.find(); 
+    res.json(users);
+});
+
+router.get('/user/:id', async (req, res) => {
+    //Traer un usuario especifico pasando el ID
+    // Esta ruta obtiene un usuario específico de la base de datos
+    // usando el método findById() del modelo UserSchema.
+    var id = req.params.id;
+    let user = await UserSchema.findById(id); 
+    res.json(user);
+
+    //Traer un usuario pasandole el email
+    // También se puede obtener un usuario por su correo electrónico.
+    // Este código está comentado y no se utiliza en este momento.
+    // Se puede descomentar si se desea habilitar esta funcionalidad.
+    // const query = UserSchema.where({ email: email });
+    // const user = await query.findOne()
+});
+
+router.post('/user', async (req, res) => {
+    //Crear un usuario
+    // Esta ruta crea un nuevo usuario en la base de datos.
+    // Primero se cifra la contraseña usando bcrypt.
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    // Se crea un nuevo objeto usuario utilizando los datos recibidos en la solicitud.
+    let user = UserSchema({
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        id: req.body.id,
+        password: hashedPassword
+    });
+
+    // Se guarda el usuario en la base de datos.
+    user.save().then((result) => {
+        res.send(result);
+    }).catch((err) => {
+        // Se manejan los errores que pueden ocurrir al guardar el usuario.
+        // Si el correo electrónico ya está registrado, se envía un mensaje de error.
+        if(err.code == 11000){
+            res.send({"status" : "error", "message" :"El correo ya fue registrado"});      
+        } else {
+            // Si ocurre algún otro error, se envía un mensaje de error genérico.
+            res.send({"status" : "error", "message" :err.message});      
+        }
+    });
+});
+
+router.patch('/user/:id', (req, res) => {
+    //Actualizar un usuario
+    // Esta ruta actualiza un usuario existente en la base de datos.
+    // Se obtiene el ID del usuario de los parámetros de la URL.
+    var id = req.params.id;
+    
+    // Se crea un objeto con los datos actualizados del usuario.
+    var updateUser = {
+        name: req.body.name,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        id: req.body.id
+    };
+
+    // Se actualiza el usuario en la base de datos utilizando findByIdAndUpdate().
+    UserSchema.findByIdAndUpdate(id, updateUser, {new: true}).then((result) => {
+        res.send(result);
+    }).catch((error) => {
+        // Se maneja cualquier error que pueda ocurrir al actualizar el usuario.
+        console.log(error);
+        res.send("Error actualizando el registro");
+    });
+});
+
+router.delete('/user/:id', (req, res) => {
+    //Eliminar un usuario
+    // Esta ruta elimina un usuario de la base de datos.
+    // Se obtiene el ID del usuario de los parámetros de la URL.
+    var id = req.params.id;
+
+    // Se elimina el usuario de la base de datos utilizando deleteOne().
+    UserSchema.deleteOne({_id: id}).then(() => {
+        res.json({"status": "success", "message": "User deleted successfully"});
+    }).catch((error) => {
+        // Se maneja cualquier error que pueda ocurrir al eliminar el usuario.
+        console.log(error);
+        res.json({"status": "failed", "message": "Error deleting user"});
+    });
+});
+
+router.post('/login', (req, res) => {
+    //Inicio de sesión
+    // Esta ruta maneja la autenticación del usuario.
+    // Se obtienen el correo electrónico y la contraseña de la solicitud.
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Se llama al método login del controlador de usuario para iniciar sesión.
+    userController.login(email, password).then((result) => {
+        res.send(result);
+    });
+});
+
+
+module.exports = router
